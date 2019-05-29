825a176607938eca799afb11a00d0eeb75b315af
alexlbr <alex.lobera@gmail.com>
2019-05-29 20:03:19 +0100

    adds books mock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fetch = require("node-fetch");
 const { ApolloServer, gql } = require("apollo-server");
 
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
